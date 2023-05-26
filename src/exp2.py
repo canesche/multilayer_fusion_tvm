ea9012698006ed5fedcf530faf4925288499f256
canesche <michael.canesche@gmail.com>
2023-05-26 19:17:52 -0300

    update

--- src/exp2.py
+++ src/exp2.py
@@ -1,131 +1,11 @@
-import tvm, logging, sys, os
-from tvm import te, topi, autotvm, relay
+import tvm, logging, sys, os, time
+from tvm import te, topi, autotvm
 import numpy as np
 
-from utils import auto_fusion_schedule, get_best_time, p_value
-
-#logging.getLogger('autotvm').setLevel(logging.DEBUG)
-#logging.getLogger('autotvm').addHandler(logging.StreamHandler(sys.stdout))
+from utils import auto_fusion_schedule, get_best_time, p_value, auto_tile_schedule
 
 dtype="float32"
 dev = tvm.cpu(0)
-
-
-def normal(N, H, W, CO, CI, KH, KW, stride, padding):
-
-    d_tvm = tvm.nd.array((np.random.uniform(size=(N, CI, H, W))).astype(dtype), device=dev)
-    f_tvm = tvm.nd.array((np.random.uniform(size=(CO, CI, KH, KW))).astype(dtype), device=dev)
-    b_tvm = tvm.nd.array((np.random.uniform(size=(1, W, 1))).astype(dtype), device=dev)
-
-    data = te.placeholder((N, CI, H, W), name="data")
-    kernel = te.placeholder((CO, CI, KH, KW), name="kernel")
-    bias = te.placeholder((1, W, 1), name='bias')
-
-    conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
-    relu1 = topi.nn.relu(conv1)
-
-    s = te.create_schedule(relu1.op)
-    args = [data, kernel, bias]
-
-    return s, args
-
-
-@autotvm.template("only_opt")
-def only_opt(N, H, W, CO, CI, KH, KW, stride, padding):
-
-    data = te.placeholder((N, CI, H, W), name="data")
-    kernel = te.placeholder((CO, CI, KH, KW), name="kernel")
-    bias = te.placeholder((1, W, 1), name='bias')
-
-    conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
-    relu1 = topi.nn.relu(conv1)
-
-    s = te.create_schedule(relu1.op)
-    args = [data, kernel, bias]
-
-    search_space = [1,4,8,12,16,20,24,28,32]
-
-    n, f, y, x = s[conv1].op.axis
-    rc, ry, rx = s[conv1].op.reduce_axis
-    n2, f2, y2, x2 = s[relu1].op.axis
-
-    cfg = autotvm.get_config()
-
-    cfg.define_knob("tile_f_conv1", search_space)
-    cfg.define_knob("tile_y_conv1", search_space) 
-    cfg.define_knob("tile_x_conv1", search_space)
-    cfg.define_knob("tile_rc_conv1", search_space)
-    cfg.define_knob("tile_ry_conv1", search_space)
-    cfg.define_knob("tile_rx_conv1", search_space) 
-
-    f10, f11 = s[conv1].split(f, cfg["tile_f_conv1"].val)
-    y10, y11 = s[conv1].split(y, cfg["tile_y_conv1"].val)
-    x10, x11 = s[conv1].split(x, cfg["tile_x_conv1"].val)
-    rc10, rc11 = s[conv1].split(rc, cfg["tile_rc_conv1"].val)
-    ry10, ry11 = s[conv1].split(ry, cfg["tile_ry_conv1"].val)
-    rx10, rx11 = s[conv1].split(rx, cfg["tile_rx_conv1"].val)
-
-    cfg.define_knob("tile_f_relu1", search_space)
-    cfg.define_knob("tile_y_relu1", search_space)
-    cfg.define_knob("tile_x_relu1", search_space)
-
-    f20, f21 = s[relu1].split(f2, cfg["tile_f_relu1"].val)
-    y20, y21 = s[relu1].split(y2, cfg["tile_y_relu1"].val)
-    x20, x21 = s[relu1].split(x2, cfg["tile_x_relu1"].val)
-
-    return s, args
-
-
-@autotvm.template("fusion_opt")
-def fusion_opt(N, H, W, CO, CI, KH, KW, stride, padding):
-
-    # Create the input tensor
-    data = te.placeholder((N, CI, H, W), name="data")
-    kernel = te.placeholder((CO, CI, KH, KW), name="kernel")
-    bias = te.placeholder((1, W, 1), name='bias')
-
-    conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
-    relu1 = topi.nn.relu(conv1)
-
-    # Create a TVM schedule for the computation
-    s = te.create_schedule(relu1.op)
-    args = [data, kernel, bias]
-
-    search_space = [1,4,8,12,16,20,24,28,32,36,40,44,48]
-
-    cfg = autotvm.get_config()
-
-    # merging the kernel first and then applying tile optimization works!
-    auto_fusion_schedule(s, cfg, [conv1, relu1])
-
-    n, f, y, x = s[conv1].op.axis
-    rc, ry, rx = s[conv1].op.reduce_axis
-    n2, f2, y2, x2 = s[relu1].op.axis
-
-    cfg.define_knob("tile_f_conv1", search_space)
-    cfg.define_knob("tile_y_conv1", search_space) 
-    cfg.define_knob("tile_x_conv1", search_space)
-    cfg.define_knob("tile_rc_conv1", search_space)
-    cfg.define_knob("tile_ry_conv1", search_space)
-    cfg.define_knob("tile_rx_conv1", search_space) 
-
-    f10, f11 = s[conv1].split(f, cfg["tile_f_conv1"].val)
-    y10, y11 = s[conv1].split(y, cfg["tile_y_conv1"].val)
-    x10, x11 = s[conv1].split(x, cfg["tile_x_conv1"].val)
-    rc10, rc11 = s[conv1].split(rc, cfg["tile_rc_conv1"].val)
-    ry10, ry11 = s[conv1].split(ry, cfg["tile_ry_conv1"].val)
-    rx10, rx11 = s[conv1].split(rx, cfg["tile_rx_conv1"].val)
-
-    cfg.define_knob("tile_f_relu1", search_space)
-    cfg.define_knob("tile_y_relu1", search_space)
-    cfg.define_knob("tile_x_relu1", search_space)
-
-    f20, f21 = s[relu1].split(f2, cfg["tile_f_relu1"].val)
-    y20, y21 = s[relu1].split(y2, cfg["tile_y_relu1"].val)
-    x20, x21 = s[relu1].split(x2, cfg["tile_x_relu1"].val)
-
-    return s, args
-
 
 @autotvm.template("fusion")
 def fusion(N, H, W, CO, CI, KH, KW, stride, padding):
@@ -135,20 +15,80 @@ def fusion(N, H, W, CO, CI, KH, KW, stride, padding):
     bias = te.placeholder((1, W, 1), name='bias')
 
     conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
-    relu1 = topi.nn.relu(conv1)
+    bias1 = topi.add(conv1, bias)
+    relu1 = topi.nn.relu(bias1)
 
     s = te.create_schedule(relu1.op)
     args = [data, kernel, bias]
 
     cfg = autotvm.get_config()
-
-    auto_fusion_schedule(s, cfg, [conv1, relu1])
+    auto_fusion_schedule(s, cfg, [conv1, bias1, relu1])
 
     return s, args
 
+@autotvm.template("only_opt")
+def only_opt(N, H, W, CO, CI, KH, KW, stride, padding):
+
+    data = te.placeholder((N, CI, H, W), name="data")
+    kernel = te.placeholder((CO, CI, KH, KW), name="kernel")
+    bias = te.placeholder((1, W, 1), name='bias')
+
+    conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
+    bias1 = topi.add(conv1, bias)
+    relu1 = topi.nn.relu(bias1)
+
+    s = te.create_schedule(relu1.op)
+    args = [data, kernel, bias]
+    tensors = [conv1, bias1, relu1]
+
+    cfg = autotvm.get_config()
+    search_space = [0,1,4,8,12,16,20,24,28,32]
+    auto_tile_schedule(s, cfg, tensors, search_space)
+
+    return s, args
+
+@autotvm.template("fusion_opt")
+def fusion_opt(N, H, W, CO, CI, KH, KW, stride, padding):
+
+    data = te.placeholder((N, CI, H, W), name="data")
+    kernel = te.placeholder((CO, CI, KH, KW), name="kernel")
+    bias = te.placeholder((1, W, 1), name='bias')
+
+    conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
+    bias1 = topi.add(conv1, bias)
+    relu1 = topi.nn.relu(bias1)
+
+    s = te.create_schedule(relu1.op)
+    args = [data, kernel, bias]
+    tensors = [conv1, bias1, relu1]
+
+    cfg = autotvm.get_config()
+    search_space = [0,1,4,8,12,16,20,24,28,32]
+    auto_fusion_schedule(s, cfg, tensors)
+    auto_tile_schedule(s, cfg, tensors, search_space)
+
+    return s, args
+
+def normal(N, H, W, CO, CI, KH, KW, stride, padding):
+
+    data = te.placeholder((N, CI, H, W), name="data")
+    kernel = te.placeholder((CO, CI, KH, KW), name="kernel")
+    bias = te.placeholder((1, W, 1), name='bias')
+
+    conv1 = topi.nn.conv2d(data, kernel, strides=stride, padding=padding, dilation=1, data_layout="NCHW")
+    bias1 = topi.add(conv1, bias)
+    relu1 = topi.nn.relu(bias1)
+
+    s = te.create_schedule(relu1.op)
+    args = [data, kernel, bias]
+
+    return s, args
 
 def execute_normal(N, H, W, CO, CI, KH, KW, stride, padding):
+
+    begin = time.time()
     s, args = normal(N, H, W, CO, CI, KH, KW, stride, padding)
+    final_time = time.time() - begin
 
     d_tvm = tvm.nd.array((np.random.uniform(size=(N, CI, H, W))).astype(dtype), device=dev)
     f_tvm = tvm.nd.array((np.random.uniform(size=(CO, CI, KH, KW))).astype(dtype), device=dev)
@@ -157,15 +97,15 @@ def execute_normal(N, H, W, CO, CI, KH, KW, stride, padding):
     with tvm.transform.PassContext(opt_level=0):
         mod = tvm.build(s, args=args, target="llvm", name="main")
         mod(d_tvm, f_tvm, b_tvm)
+    
     r = []
     for _ in range(3):
         evaluator = mod.time_evaluator(mod.entry_name, dev, number=20, repeat=1)
         mean_time = evaluator(d_tvm, f_tvm, b_tvm)
         r.append(mean_time.mean)
     r = np.array(r)
-    print("%s,%.6f,%.6f" %("normal", np.mean(r), np.std(r)))
+    print("%s,%.6f,%.6f,%.2f" %("normal", np.mean(r), np.std(r), final_time))
     return r
-
 
 def execute_autoTVM(tag_name, func, N, H, W, CO, CI, KH, KW, stride, padding):
 
@@ -176,13 +116,15 @@ def execute_autoTVM(tag_name, func, N, H, W, CO, CI, KH, KW, stride, padding):
     task = autotvm.task.create(tag_name, args=(N, H, W, CO, CI, KH, KW, stride, padding), target="llvm")
     #print(task.config_space)
 
+    begin = time.time()
     measure_option = autotvm.measure_option(
         builder=autotvm.LocalBuilder(),
         runner=autotvm.LocalRunner(repeat=5, number=10, min_repeat_ms=100, enable_cpu_cache_flush=True),
     )
-
     tuner = autotvm.tuner.XGBTuner(task)
-    record_file = tag_name+".log"
+    final_time = time.time() - begin
+    
+    record_file = tag_name+"%dx%d.log" %(H,W)
     
     if os.path.isfile(record_file):
         os.remove(record_file)
@@ -221,7 +163,7 @@ if __name__ == "__main__":
     KH, KW = (3, 3)
     stride = (1, 1)
     padding = (1, 1)
-    interval = [1024, 2048]
+    interval = [256,512,1024,2048]
 
     for i in interval:
         H, W = (i, i)
@@ -229,9 +171,9 @@ if __name__ == "__main__":
 
         r_normal = execute_normal(N, H, W, CO, CI, KH, KW, stride, padding)
         r_fusion = execute_autoTVM("fusion", fusion, N, H, W, CO, CI, KH, KW, stride, padding)
-        #r_only_opt = execute_autoTVM("only_opt", only_opt, N, H, W, CO, CI, KH, KW, stride, padding)
+        r_only_opt = execute_autoTVM("only_opt", only_opt, N, H, W, CO, CI, KH, KW, stride, padding)
         #r_fusion_opt = execute_autoTVM("fusion_opt", fusion_opt, N, H, W, CO, CI, KH, KW, stride, padding)
 
         print(p_value(r_normal, r_fusion))
-        #print(p_value(r_normal, r_only_opt))
+        print(p_value(r_normal, r_only_opt))
         #print(p_value(r_normal, r_fusion_opt))
---
+++ src/utils.py
@@ -0,0 +1,93 @@
+from tvm import autotvm
+from scipy import stats
+import numpy as np
+
+def print_var_name(variable):
+ for name in globals():
+     if eval(name) == variable:
+        return name
+
+def auto_fusion_schedule(s, cfg, tensors):
+    for t in range(0,len(tensors)-1):
+        cfg = autotvm.get_config()
+        name = "fuse_%d" % t
+
+        size_schedule = 8 if t == 0 or t == len(tensors)-1 else 9
+
+        cfg.define_knob(name, [i for i in range(size_schedule)])
+
+        assert t < len(tensors)
+
+        actual_tensor = tensors[t]
+        next_tensor = tensors[t+1]
+
+        if cfg[name].val == 1:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[1])
+        elif cfg[name].val == 2:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[2])
+        elif cfg[name].val == 3:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[3])
+        elif cfg[name].val == 4:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[1])
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[2])
+        elif cfg[name].val == 5:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[1])
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[3])
+        elif cfg[name].val == 6:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[2])
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[3])
+        elif cfg[name].val == 7:
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[1])
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[2])
+            s[actual_tensor].compute_at(s[next_tensor], next_tensor.op.axis[3])
+        elif size_schedule == 9 and cfg[name].val == 8 and cfg["fuse_%d" % t-1].val == 0:
+            s[actual_tensor].compute_inline()
+
+def auto_tile_schedule(s, cfg, tensors, search_space):
+    for t in range(0,len(tensors)):
+        actual_tensor = tensors[t]
+        name_tensor = str(t)
+        
+        axis = s[actual_tensor].op.axis
+        reduce_axis = s[actual_tensor].op.reduce_axis
+        
+        for i in range(len(axis)):
+            name_axis = axis[i].var
+            #dom_axis = axis[i].dom
+            name_opt = "tile_%s_%s" %(name_axis, name_tensor)
+            cfg.define_knob(name_opt, search_space)
+            if cfg[name_opt].val != 0:
+                _, _ = s[actual_tensor].split(axis[i], cfg[name_opt].val)
+        
+        for i in range(len(reduce_axis)):
+            name_axis = reduce_axis[i].var
+            #dom_axis = axis[i].dom
+            name_opt = "tile_%s_%s" %(name_axis, name_tensor)
+            cfg.define_knob(name_opt, search_space)
+            if cfg[name_opt].val != 0:
+                _, _ = s[actual_tensor].split(reduce_axis[i], cfg[name_opt].val)
+        #print(s[actual_tensor].op)
+        #print(s[actual_tensor].op.axis)
+        #print(s[actual_tensor].op.reduce_axis)
+
+def get_best_time(log, ms=True):
+    import json
+
+    f = open(log, "r")
+    best_avg = 9999.0
+    best_cfg = {}
+    for line in f.readlines():
+        data = json.loads(line)
+        r = np.mean(data["result"][0])
+        if (np.mean(best_avg) > r):
+            best_avg = data["result"][0]
+            best_cfg = data["config"]["entity"]
+    f.close()
+
+    if ms: # convet to ms
+        best_avg *= 1000
+    return best_avg, best_cfg
+
+
+def p_value(elem_1, elem_2):
+    return stats.ttest_ind(elem_1, elem_2).pvalue
